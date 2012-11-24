a0cfdd516704b43a4f231c7fd61ac9c618d5e854
andrey <gryeaht111@gmail.com>
2012-11-24 11:21:36 +0400

    Initial Commit

---
+++ ids_head/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  ids_head
+//
+//  Created by andrey on 24.11.12.
+//
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
